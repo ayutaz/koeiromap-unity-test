0bda6d0db782ebf0c7781648d2ea44acef88b90e
yousan <ka1357amnbpdr@gmail.com>
2023-03-04 03:07:17 +0900

    feat: 文字指定した音声ファイルの生成と再生

---
+++ Assets/_KoeiromapTest/Scripts/GetVoice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace _KoeiromapTest
+{
+    public class GetVoice
+    {
+        private const string URL = "http://koeiromap-api.rinna.jp/api/inference/";
+        public static async UniTask<VoiceResponse> Voice(string text)
+        {
+            var voiceParam = new VoiceParam()
+            {
+                text = text,
+                speaker_x = 2.44f,
+                speaker_y = 2.88f,
+                style = "talk"
+            };
+            var json = JsonUtility.ToJson(voiceParam);
+            var bodyRaw = Encoding.UTF8.GetBytes(json);
+            var request = new UnityWebRequest(URL, "POST");
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            await request.SendWebRequest();
+            return JsonUtility.FromJson<VoiceResponse>(request.downloadHandler.text);
+        }
+
+        public static string VoiceBase64Data(VoiceResponse voiceResponse)
+        {
+            var audio = voiceResponse.audio;
+            return audio[(audio.IndexOf(",", StringComparison.Ordinal) + 1)..];
+        }
+    }
+
+    public class VoiceParam
+    {
+        public string text;
+        public float speaker_x;
+        public float speaker_y;
+        public string style;
+    }
+
+    public class VoiceResponse
+    {
+        public string audio;
+        public string phonemes;
+        public int seed;
+    }
+}
---
+++ Assets/_KoeiromapTest/Scripts/VoiceManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace _KoeiromapTest
+{
+    public class VoiceManager : MonoBehaviour
+    {
+        private string _filePath;
+        private AudioSource _audioSource;
+        private void Awake()
+        {
+            _filePath = Path.Combine(Application.persistentDataPath, "voice.wav");
+            _audioSource = GetComponent<AudioSource>();
+            Debug.Log("filePath: " + _filePath);
+        }
+
+        private async void Start()
+        {
+            var voiceResponse = await GetVoice.Voice("こんにちは");
+            var voiceBase64 = GetVoice.VoiceBase64Data(voiceResponse);
+            // Base64文字列からbyte配列を作成
+            var bytes = Convert.FromBase64String(voiceBase64);
+
+            // byte配列をfloat配列に変換する
+            var floatSamples = new float[bytes.Length / 2];
+            for (int i = 0; i < floatSamples.Length; i++)
+            {
+                var sample = BitConverter.ToInt16(bytes, i * 2);
+                floatSamples[i] = sample / 32768f;
+            }
+
+            // AudioClipを作成する
+            var clip = AudioClip.Create("clip", floatSamples.Length, 1, 44100, false);
+            clip.SetData(floatSamples, 0);
+
+            // AudioClipをAudioSourceに設定
+            _audioSource.clip = clip;
+
+            // 再生
+            _audioSource.Play();
+        }
+    }
+}
