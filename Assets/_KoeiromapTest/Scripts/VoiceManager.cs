7a4ee086548aa308ee09621a1b43d910417783fd
yousan <ka1357amnbpdr@gmail.com>
2023-03-04 20:16:14 +0900

    fix: base64からAudioClipへの変換処理の修正
    生成された音が割れていた問題の修正

--- Assets/_KoeiromapTest/Scripts/VoiceManager.cs
+++ Assets/_KoeiromapTest/Scripts/VoiceManager.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 namespace _KoeiromapTest
@@ -27,26 +29,32 @@ namespace _KoeiromapTest
         {
             var voiceResponse = await GetVoice.Voice(text);
             var voiceBase64 = GetVoice.VoiceBase64Data(voiceResponse);
-            // Base64文字列からbyte配列を作成
-            var bytes = Convert.FromBase64String(voiceBase64);
-
-            // byte配列をfloat配列に変換する
-            var floatSamples = new float[bytes.Length / 2];
-            for (var i = 0; i < floatSamples.Length; i++)
-            {
-                var sample = BitConverter.ToInt16(bytes, i * 2);
-                floatSamples[i] = sample / 32768f;
-            }
-
-            // AudioClipを作成する
-            var clip = AudioClip.Create("clip", floatSamples.Length, 1, 44100, false);
-            clip.SetData(floatSamples, 0);
 
             // AudioClipをAudioSourceに設定
-            _audioSource.clip = clip;
+            _audioSource.clip = await ConvertBase64ToAudioClip(voiceBase64);
 
             // 再生
             _audioSource.Play();
+        }
+
+        private static async UniTask<AudioClip> ConvertBase64ToAudioClip(string base64EncodedMp3String)
+        {
+            var audioBytes = Convert.FromBase64String(base64EncodedMp3String);
+            var tempPath = Application.persistentDataPath + "tmpMP3Base64.wav";
+            await File.WriteAllBytesAsync(tempPath, audioBytes);
+            var request = UnityWebRequestMultimedia.GetAudioClip(tempPath, AudioType.WAV);
+            var asyncOperation = request.SendWebRequest();
+            await asyncOperation;
+            if (request.result.Equals(UnityWebRequest.Result.ConnectionError))
+            {
+                Debug.LogError(request.error);
+                return null;
+            }
+
+            var content = DownloadHandlerAudioClip.GetContent(request);
+            request.Dispose();
+            return content;
+
         }
     }
 }
